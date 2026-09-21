c546a0a2d5c73c560d8f2ce4ab7616fbf5794e0d
Danivh77 <laurad.vegah@uqvirtual.edu.co>
2026-09-20 19:51:30 -0500

    Update MyMusic.exs
    El menú del CRUD de las canciones

--- MyMusic.exs
+++ MyMusic.exs
@@ -32,7 +32,7 @@ defmodule MyMusic do
       %{
         id: 1,
         nombre: "Para Entrenar",
-        descripcion: "Canciones para escuchar mientras hago ejercicio",
+        descripcion: "Canciones para escuchar mientras entreno",
         canciones: [2, 3]
       },
       %{
@@ -44,6 +44,54 @@ defmodule MyMusic do
     ]
 
     menu(canciones, playlists)
+  end
+
+  def menu_canciones(canciones) do
+
+    Util.mostrar_mensaje("""
+
+    ==============================
+              CANCIONES
+    ==============================
+    1. Crear canción
+    2. Ver canciones
+    3. Actualizar canción
+    4. Eliminar canción
+    5. Buscar canción
+    6. Volver
+
+    """)
+
+    opcion = Util.ingresar("Seleccione una opción: ", :entero)
+
+    case opcion do
+      1 ->
+        nuevas_canciones = crear_cancion(canciones)
+        menu_canciones(nuevas_canciones)
+
+      2 ->
+        mostrar_canciones(canciones)
+        menu_canciones(canciones)
+
+      3 ->
+        nuevas_canciones = actualizar_cancion(canciones)
+        menu_canciones(nuevas_canciones)
+
+      4 ->
+        nuevas_canciones = eliminar_cancion(canciones)
+        menu_canciones(nuevas_canciones)
+
+      5 ->
+        buscar_cancion(canciones)
+        menu_canciones(canciones)
+
+      6 ->
+        canciones
+
+      _ ->
+        Util.mostrar_mensaje("Opción inválida.")
+        menu_canciones(canciones)
+    end
   end
 
 end
